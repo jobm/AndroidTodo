4412a62d5bf0b9c5460bac4c8b2451ebd5666e06
JobM <jobmwaniki18@gmail.com>
2015-10-25 16:58:48 +0300

    added db package

---
+++ app/src/main/java/com/example/job/helloworld/db/TaskContract.java
@@ -0,0 +1,18 @@
+package com.example.job.helloworld.db;
+
+/**
+ * Created by Job on 10/25/2015.
+ */
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.TodoList.db.tasks";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
+}
